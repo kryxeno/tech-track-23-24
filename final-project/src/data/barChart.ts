fdc97387956b866146cb7e1675260cafa9f0a286
Mr Bats <timvaningen63@gmail.com>
2023-11-10 12:08:05 +0100

    added enter update exit do bar

--- final-project/src/data/barChart.ts
+++ final-project/src/data/barChart.ts
@@ -60,12 +60,32 @@ const createBarChart = (
   svg
     .selectAll(".bar")
     .data(barData)
-    .join("rect")
+    .join(
+      (enter) =>
+        enter
+          .append("rect")
+          .attr("fill", "green")
+          .attr("opacity", 1)
+          .attr("x", (_, i) => xScale(x[i] as any) as any)
+          .attr("y", -dimensions.height)
+          .attr("width", xScale.bandwidth()),
+      (update) =>
+        update
+          .attr("opacity", 1)
+          .transition()
+          .duration(1000)
+          .attr("fill", "yellow"),
+      (exit) =>
+        exit
+          .attr("opacity", 1)
+          .attr("fill", "red")
+          .transition()
+          .duration(1000)
+          .attr("opacity", 0)
+          .remove()
+    )
     .attr("class", "bar")
     .style("transform", "scale(1, -1)")
-    .attr("x", (d, i) => xScale(x[i] as any) as any)
-    .attr("y", -dimensions.height)
-    .attr("width", xScale.bandwidth())
     .on("mouseenter", (event, value) => {
       const bar = d3.select(event.currentTarget)
 
@@ -81,13 +101,89 @@ const createBarChart = (
         .attr("y", yScale(value) - 12)
         .attr("opacity", 1.0)
     })
-    .on("mouseleave", (value, index) => {
+    .on("mouseleave", () => {
       svg.select(".tooltip").remove()
     })
     .transition()
+    .delay(1000)
     .duration(1000)
+    .attr("x", (_, i) => xScale(x[i] as any) as any)
+    .attr("y", -dimensions.height)
+    .attr("width", xScale.bandwidth())
     .attr("fill", colorScale)
     .attr("height", (d) => dimensions.height - yScale(d))
 }
+// export const updateBarChart = (
+//   svgRef: MutableRefObject<SVGSVGElement | null>,
+//   data: {
+//     [key: string]: object[]
+//   },
+//   dimensions: { width: number; height: number }
+// ) => {
+//   if (!data) return
+//   const svg = d3.select(svgRef.current)
+
+//   const x: number[] = []
+//   const barData: number[] = []
+
+//   console.log({ x, barData })
+
+//   Object.keys(data).forEach((key) => {
+//     x.push(parseInt(key))
+//   })
+
+//   Object.keys(data).forEach((key) => {
+//     barData.push(data[key].length)
+//   })
+
+//   const xScale = d3
+//     .scaleBand()
+//     .domain(x as unknown as string[])
+//     .range([0, dimensions.width])
+//     .padding(0.25)
+
+//   const yScale = d3.scaleLinear().domain([0, 50]).range([dimensions.height, 0])
+
+//   const colorScale = d3
+//     .scaleLinear()
+//     .domain([0, 50])
+//     .range(["#ec8431", "blue"] as any)
+
+//   svg
+//     .selectAll(".bar")
+//     .data(barData)
+//     .join(
+//       (enter) => enter.append("rect"),
+//       (update) => update
+//     )
+//     .attr("class", "bar")
+//     .style("transform", "scale(1, -1)")
+//     .attr("x", (d, i) => xScale(x[i] as any) as any)
+//     .attr("y", -dimensions.height)
+//     .attr("width", xScale.bandwidth())
+//     .on("mouseenter", (event, value) => {
+//       const bar = d3.select(event.currentTarget)
+
+//       svg
+//         .selectAll(".tooltip")
+//         .data([value])
+//         .join((enter) => enter.append("text").attr("y", yScale(value) - 8))
+//         .attr("class", "tooltip")
+//         .text(value)
+//         .attr("x", parseInt(bar.attr("x")) + xScale.bandwidth() / 2)
+//         .attr("text-anchor", "middle")
+//         .transition()
+//         .attr("y", yScale(value) - 12)
+//         .attr("opacity", 1.0)
+//     })
+//     .on("mouseleave", (value, index) => {
+//       svg.select(".tooltip").remove()
+//     })
+//     .transition()
+//     .duration(1000)
+//     .attr("fill", colorScale)
+//     .attr("height", (d) => dimensions.height - yScale(d))
+
+// }
 
 export default createBarChart
